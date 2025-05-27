123bd3ff18bd6624b9c32ba4e15e1dccdd49ed29
Ekwe Emmanuel <ekwea753@gmail.com>
2025-05-27 15:22:13 +0100

    Add files via upload
    phase gate assesment

---
+++ python/phaseGate/roit_cipher.py
@@ -0,0 +1,15 @@
+def encrypt_text(*args):
+	user_input = input("Enter a word: ")
+	user-input.lower
+
+def change_alphabet(character_check):
+	new_position = 0
+	alphabets = {a, b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+	alphabets.lower
+	
+for row in range(alphabets.length):
+	for reverse in range(user_input, row.length):
+		if(alphabets == row):
+			user_input = alphabets.replace()
+			print(user_input)
+
---
+++ python/phaseGate/to_do_list.py
@@ -0,0 +1,29 @@
+def to_do_list():
+	while(True):
+		print("Welcome to my to_do list")
+		print("""
+			1. Add a task
+			2. View all tasks
+			3. Mark a task as complete
+			4. Delete a task
+			5. Exit
+
+		""")
+make_choice = input("Choose between 1 - 5: ")
+if make_choice == "0":
+	print("Enter a valid number")
+elif make_choice == "1":
+	print("Add a task")
+	change == make_choice.replace()
+elif make_choice == "2":
+	print("View all tasks")
+elif make_choice == "3":
+	print("Mark a task as complete")
+elif make_choice == "4":
+	print("Delete a task")
+else:
+	print(exit)
+
+
+
+        
