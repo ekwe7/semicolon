5a577b283070ffbe688758abe26e6812d06dfb9e
Ekwe Emmanuel <ekwea753@gmail.com>
2025-05-30 11:43:53 +0100

    Add files via upload

---
+++ javaScript/validateAmtCard.js
@@ -0,0 +1,22 @@
+function validateAmtCard(arrayNumber){
+
+let arrayNumber = [];
+
+
+for(let index = 0; index < arrayNumber.length; index++){
+	valueCheck = valueCheck + index;		
+	}
+	
+if(valueCheck == 4 && card == 16){
+console.log(Visa Card);
+}else
+if(card == 5 && card == 15){
+console.log(American Express);
+}
+
+
+}
+
+
+let array = validateAmtCard.[4,4,2,7,8,4,2,9,3,0,2,5,0,1,7];
+console.log(array);
